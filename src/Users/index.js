a113d4ececaf474c366c92d564d8d15fd488806a
Enzo BLANCHON <blanchonenzo@gmail.com>
2021-05-25 15:46:44 +0200

    setFavorite

--- src/Users/index.js
+++ src/Users/index.js
@@ -6,6 +6,25 @@ class Users {
     getAll(sipExtension, page, number) {
         return this.API.get("users/" + sipExtension + "/" + page + "/" + number + "/");
     }
+
+    setFavorite(sipExtension, favorite) {
+        return new Promise(async (resolve, reject) => {
+            this.API.post("users/favorites", {
+                "sipExtension": sipExtension,
+                "favorite" : favorite,
+            }).then((response) => {
+                resolve({
+                    success: true,
+                    response: response
+                })
+            }).catch((error) => {
+                reject({
+                    success: false,
+                    error: error
+                });
+            })
+        })
+    }
 }
 
 module.exports = Users;
