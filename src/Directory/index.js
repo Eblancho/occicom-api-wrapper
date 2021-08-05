96d4d96c812f6f4eb3d576d9ab4177843255021e
Enzo BLANCHON <blanchonenzo@gmail.com>
2021-08-05 17:59:18 +0200

    spec directory

--- src/Directory/index.js
+++ src/Directory/index.js
@@ -6,6 +6,10 @@ class Directory {
     getAll(sipExtension, page, number) {
         return this.API.get("directory/" + sipExtension + "/all/" + page + "/" + number + "/");
     }
+
+    getDirectory(sipExtension, directory, page, number) {
+        return this.API.get("directory/" + sipExtension + "/" + directory +"/" + page + "/" + number + "/");
+    }
 }
 
 module.exports = Directory;
